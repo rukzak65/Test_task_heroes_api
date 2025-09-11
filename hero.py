b08743797df733515de6a93747016c81dc692bc9
Chanturia <chanturia007@mail.ru>
2025-09-11 18:51:21 +0300

    new func get_tallest_hero

--- hero.py
+++ hero.py
@@ -15,6 +15,35 @@ def parse_height_cm(height_field):
         return None
 
 
+def get_tallest_hero(gender: str, has_work: bool = False, heroes: list = None):
 
+    if heroes is None:
+        resp = requests.get(API_URL, timeout=10)
+        if resp.status_code != 200:
+            raise RuntimeError("Не удалось получить данные от API")
+        heroes = resp.json()
+
+    tallest = None
+
+    for h in heroes:
+        # фильтрация по полу
+        if h.get("appearance", {}).get("gender", "").lower() != gender.lower():
+            continue
+
+        # фильтрация по работе
+        occupation = (h.get("work") or {}).get("occupation")
+        if has_work and (not occupation or occupation.strip() in ["-", ""]):
+            continue
+
+        # рост
+        height_cm = parse_height_cm(h.get("appearance", {}).get("height"))
+        if height_cm is None:
+            continue
+
+        # выбираем самого высокого
+        if tallest is None or height_cm > tallest["height_cm"]:
+            tallest = {"name": h.get("name"), "height_cm": height_cm}
+
+    return tallest
 
 
