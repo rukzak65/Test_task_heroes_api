6d1e120ae3ec855325afa91ea622ebd49e90b3fd
Chanturia <chanturia007@mail.ru>
2025-09-19 12:33:36 +0300

    update conftest

--- conftest.py
+++ conftest.py
@@ -1,2 +1,17 @@
 import pytest
 import requests
+
+
+API_URL = "https://akabab.github.io/superhero-api/api/all.json"
+REQUEST_TIMEOUT = 5
+@pytest.fixture(scope="session")
+def heroes_api():
+    try:
+        r = requests.get(API_URL, timeout=REQUEST_TIMEOUT)
+        r.raise_for_status()
+        data = r.json()
+        if not isinstance(data, list):
+            pytest.skip("API вернул не список.")
+        return data
+    except Exception as e:
+        pytest.skip(f"Не удалось достучаться до API: {e}")
